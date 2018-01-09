704deda0111f2f48646f366677023c0e7cbea36c
chmzh <28458942@qq.com>
2018-01-09 17:14:59 +0800

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -27,3 +27,51 @@ def func2(a):
 func2()
 
 decorator(func2)()
+
+
+
+def t(fun):
+    def onCall(*args,**kwargs):
+        result = fun(args)
+        print("t(),",result)
+    return onCall
+
+def func(args): ...
+
+
+func = t(func)
+
+class Person:
+    class Name:
+        def __get__(self, instance, owner):
+            return instance._name
+
+        def __set__(self, instance, value):
+            instance._name = value
+        def __delete__(self,instance):
+            del instance._name
+
+
+    name = Name()
+    def __init__(self,name):
+        self._name = name
+        pass
+    @t
+    def test(self):
+        return 1
+        pass
+
+
+p = Person("test")
+p2 = Person("test2")
+
+p.name = "test1"
+
+p.test()
+print(p.name)
+
+# del p.name
+print(p2.name)
+
+testFun = getattr(p,"test")
+testFun()
